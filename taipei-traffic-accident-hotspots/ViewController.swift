e178140da47e4b3038a331d8cd13d1e348856e17
Hill <angelblue065@gmail.com>
2022-08-05 13:58:34 +0800

    first commit

---
+++ taipei-traffic-accident-hotspots/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  taipei-traffic-accident-hotspots
+//
+//  Created by Lin Hill on 2022/8/5.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
