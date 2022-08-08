5d5814e97f8537860a5bef9c5149d5d5f826d84c
Hill <angelblue065@gmail.com>
2022-08-08 18:27:22 +0800

    fix : error

--- taipei-traffic-accident-hotspots/Model/WebServices.swift
+++ taipei-traffic-accident-hotspots/Model/WebServices.swift
@@ -1,26 +1,26 @@
 import Foundation
 
-class WebServices {
-    
-    static func loadData(completionHandler: @escaping (Event?, Bool?) -> Void) {
-        Network.loadJSONFile(named: "WWDCAttendees", type: [Attendee].self) { (attendees, error) in
-            guard error == nil else {
-                completionHandler(nil, false)
-                return
-            }
-            
-            Network.loadJSONFile(named: "WWDCSessions", type: [Session].self) { (sessions, error) in
-                guard error == nil else {
-                    completionHandler(nil, false)
-                    return
-                }
-                
-                let event = Event()
-                event.attendees = attendees ?? []
-                event.sessions = sessions ?? []
-                
-                completionHandler(event, true)
-            }
-        }
-    }
-}
+//class WebServices {
+//    
+//    static func loadData(completionHandler: @escaping (Event?, Bool?) -> Void) {
+//        Network.loadJSONFile(named: "WWDCAttendees", type: [Attendee].self) { (attendees, error) in
+//            guard error == nil else {
+//                completionHandler(nil, false)
+//                return
+//            }
+//            
+//            Network.loadJSONFile(named: "WWDCSessions", type: [Session].self) { (sessions, error) in
+//                guard error == nil else {
+//                    completionHandler(nil, false)
+//                    return
+//                }
+//                
+//                let event = Event()
+//                event.attendees = attendees ?? []
+//                event.sessions = sessions ?? []
+//                
+//                completionHandler(event, true)
+//            }
+//        }
+//    }
+//}
